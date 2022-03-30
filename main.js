cbfa1a5c6bacda26607c05cafd14d34770fc0a57
LorenzoPrimogeri <96630976+LorenzoPrimogeri@users.noreply.github.com>
2022-03-30 14:43:38 +0200

    added computer message after user message

--- main.js
+++ main.js
@@ -1,13 +1,3 @@
-/* Consegna
-Milestone 1
-Replica della grafica con la possibilità di avere messaggi scritti dall’utente 
-(verdi) e dall’interlocutore (bianco) assegnando due classi CSS diverse
-Visualizzazione dinamica della lista contatti: 
-tramite la direttiva v-for, visualizzare nome e immagine di ogni contatto
-QUINDI solo la parte a sinistra sarà dinamica (per mostrare la lista contatti), 
-mentre a destra ci sarà HTML statico, uguale a quello presentato nel layout
- */
-
 const contacts = [
     {
         name: 'Michele',
